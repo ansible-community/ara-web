b58c5feb54ec1e87a04a7b05c29fd772015aca2c
Guillaume Vincent <gvincent@redhat.com>
2019-01-09 15:20:29 +0100

    Run unit test in zuul
    Change-Id: I0d0b57a576d1a6faba9121236b87653b6ab973d6

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import axiosMockAdapter from "axios-mock-adapter";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import App from "./App";
+
+const axiosMock = new axiosMockAdapter(axios);
+
+it("renders without crashing", () => {
+  axiosMock.onGet("config.json").reply(200, {
+    apiURL: "http://localhost:8000"
+  });
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/config/configActions.test.js
+++ src/config/configActions.test.js
@@ -1,13 +1,11 @@
 import * as actions from "./configActions";
-import * as types from "./playbooksActionsTypes";
+import * as types from "./configActionsTypes";
 
 it("setConfig", () => {
   const config = { apiURL: "http://example.org" };
-  const expectedActions = [
-    {
-      type: types.SET_CONFIG,
-      config
-    }
-  ];
+  const expectedActions = {
+    type: types.SET_CONFIG,
+    config
+  };
   expect(actions.setConfig(config)).toEqual(expectedActions);
 });
